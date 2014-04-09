6d2ac898152f5781ffdbe8d2dafdca7ce35b2ea9
Marisa Smith <priparadigm@gmail.com>
2014-04-09 02:11:03 -0500

    implemented text-fetching

--- wfc2.0.py
+++ wfc2.0.py
@@ -5,8 +5,30 @@
 # imports
 # initialize global variables
 
-# gather text to be analyzed
+       
 # remove whitespace and unnecessary symbols from text
 # tally words
 # determine the case of a word
-# display or store results
+# store results
+
+
+'''Fetch raw text from user.'''
+while True:
+    # have user choose between pasting and filename
+    preference = str(input("Please select:\n"
+                           "1. Paste text\n"
+                           "2. Parse a .txt file.\n"
+                           ">> "))
+    if preference == "1":
+        rawText = str(input("\nPlease paste text below:\n"))
+        break
+    elif preference == "2":
+        # eventually have program check to see if file exists (except FileNotFoundError)
+        filename = str(input("\nFile should be in the same "
+                             "directory as this program.\n"
+                             "Enter filename: "))
+        fileToParse = open(filename, "r")
+        rawText = fileToParse.read()
+        break
+    else:
+        print("Invalid input. Restarting...\n")
