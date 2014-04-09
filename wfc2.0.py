ff1aa881857f80966f771dc58a027e8d74d7af74
Marisa Smith <priparadigm@gmail.com>
2014-04-09 00:46:02 -0500

    version 2.0 psuedocode

---
+++ wfc2.0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# written in Python 3.3 by Marisa Smith (GitHub: @pparadigm)
+# developed on Windows 8
+
+# imports
+# initialize global variables
+
+# gather text to be analyzed
+# remove whitespace and unnecessary symbols from text
+# tally words
+# determine the case of a word
+# display or store results
