cf482c93366ae2b3bc48b075c9d14e2afdbb3184
Marisa Smith <priparadigm@gmail.com>
2014-04-13 22:07:57 -0500

    implemented word extraction

--- wfc2.0.py
+++ wfc2.0.py
@@ -12,9 +12,38 @@
 # store results
 
 
-'''Fetch raw text from user.'''
+def wordExtractor():
+    '''Extract words from a string by removing whitespace and unnecessary
+       symbols.
+
+       Note: will split numbers like 1,337 and 1.337 at the decimal and
+       comma.'''
+    word = ''
+    words = []
+    cut = False
+    for character in rawText:
+        # The rationale: Not all non-alphabetic characters appear outside of
+        # words, for example: the hyphen in 'non-alphanumeric,' the apostrophe
+        # in 'don't,' and the numbers in a word such as 'A19,' which may
+        # designate a room number. It is my intent that this function extracts
+        # words as humans read them from literature, in their full form.
+           # Characters:
+        if (character.isalnum() or
+           # Symbols:
+            character == "'" or character == '’' or character == "-"):
+            word += character
+        # Ensures skipping of empty 'words'
+        elif len(word) >= 1:
+            words.append(word)
+            word = ''
+        else:
+            continue
+    return words
+
+
+'''Program initialization loop: fetch raw text from user.'''
 while True:
-    # have user choose between pasting and filename
+    # Have user choose between pasting and filename
     preference = str(input("Please select:\n"
                            "1. Paste text\n"
                            "2. Parse a .txt file.\n"
@@ -23,7 +52,7 @@ while True:
         rawText = str(input("\nPlease paste text below:\n"))
         break
     elif preference == "2":
-        # eventually have program check to see if file exists (except FileNotFoundError)
+        # Eventually have program check to see if file exists (except FileNotFoundError)
         filename = str(input("\nFile should be in the same "
                              "directory as this program.\n"
                              "Enter filename: "))
@@ -32,3 +61,5 @@ while True:
         break
     else:
         print("Invalid input. Restarting...\n")
+
+wordList = wordExtractor()
